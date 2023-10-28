a9b9f0cefb5aedceb766a89a574472df9d8a1357
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-10-28 13:44:14 +0600

    add event section

--- src/Components/Pages/HomePage/Event/Event.jsx
+++ src/Components/Pages/HomePage/Event/Event.jsx
@@ -1,25 +1,24 @@
 import moment from "moment/moment";
 import Marquee from "react-fast-marquee";
+import SectionTitle from "../../Shared/SectionTitle";
 
 const Event = () => {
     return (
         <div className="max-w-7xl mx-auto mt-5 p-3">
-            <hr />
-            {/* Upcoming Events section*/}
-            <h2
-                className='text-center fs-1 fw-bolder text-secondary pt-4'>
-                Upcoming Events
-                <p
-                    className='fs-6 mt-3 text-warning'>
-                    {
-                        moment().format("dddd, MMMM D, YYYY")
-                    }
-                </p>
-            </h2>
+            <SectionTitle
+                heading={"Upcoming Events"}
+            ></SectionTitle>
+            <p
+                className='text-base mt-5 text-yellow-400 font-black text-center'>
+                {
+                    moment().format("dddd, MMMM D, YYYY")
+                }
+            </p>
+
             {/* live date */}
             {/* Marquee start */}
             <Marquee
-                className='fw-bold fs-6 mt-4 mb-5'>
+                className='font-semibold text-base mt-6 mb-5'>
                 Taste the Old and New Napa Valley and
                 Weekend with Healthy & Excellent Breakfasts and
                 Taste the Linguini Vineyards Cabernet Avignon
@@ -27,16 +26,16 @@ const Event = () => {
             {/* Marquee end */}
             {/* Upcoming events start */}
 
-            <div className="grid grid-cols-12">
+            <div className="grid grid-cols-12 mt-10">
 
                 <div className='col-span-8 mt-5'>
-                    <h3 className='text-warning fw-bold'>
-                        06/08/2024 — 12/08/024
+                    <h3 className='text-yellow-400 font-black text-2xl'>
+                        06/08/2024 — 12/08/2024
                     </h3>
-                    <h2 className='fw-bolder'>
+                    <h2 className='text-4xl font-bold mt-4'>
                         Taste the Old and Chicken Biryani
                     </h2>
-                    <p className='mt-4 fw-bold text-secondary'>
+                    <p className='mt-6 font-bold text-gray-500 my-4'>
                         If the chicken in a pan, adding chopped shallot, a little soda to sweeten it the carrots, and then chopping
                         <br />
                         fine, and the size of recipes for at my best; and well cooked, taking on the sauce and let it thickens;
@@ -44,8 +43,7 @@ const Event = () => {
                         add…
                     </p>
                     <button
-                        type="button"
-                        className="btn btn-dark fs-5 fw-bolder mt-3">
+                        className="bg-gray-800 text-white font-black py-3 px-6 rounded-xl text-center mt-5">
                         View More
                     </button>
                 </div>
@@ -59,37 +57,34 @@ const Event = () => {
 
             </div>
 
-            <div className='grid grid-cols-12 mt-5'>
+            <div className='grid grid-cols-12 mt-10'>
 
                 <div className="col-span-4">
                     <img
-                        className='w-100 mt-5'
+                        className='w-100 mt-5 rounded-md'
                         src="https://img.freepik.com/free-psd/delicious-food-menu-social-media-template_505751-3001.jpg?w=740&t=st=1683034680~exp=1683035280~hmac=426f6798549456db26e7c032e30cce9b6bb301bd0b3c229dd127f612d8d2b12c"
                         alt="" />
                 </div>
 
 
-                <div className='col-span-8 mt-5 ms-2'>
-                    <h3 className='text-warning fw-bold'>
-                        06/08/2024 — 12/08/024
+                <div className='col-span-8 mt-5 ms-5'>
+                    <h3 className='text-yellow-400 font-black text-2xl'>
+                        06/08/2024 — 12/08/2024
                     </h3>
-                    <h2 className='fw-bolder'>
+                    <h2 className='text-4xl font-bold mt-4'>
                         Taste the Old and Chicken Biryani
                     </h2>
-                    <p className='mt-4 fw-bold text-secondary'>
+                    <p className='mt-6 font-bold text-gray-500 my-4'>
                         If the chicken in a pan, adding chopped shallot, a little soda to sweeten it the carrots, and then chopping
                         <br />
                         fine, and the size of recipes for at my best; and well cooked, taking on the sauce and let it thickens;
                         <br />
                         add…
                     </p>
-                    <div className='d-flex justify-content-end'>
-                        <button
-                            type="button"
-                            className="btn btn-dark fs-5 fw-bolder mt-1">
-                            View More
-                        </button>
-                    </div>
+                    <button
+                        className="bg-gray-800 text-white font-black py-3 px-6 rounded-xl text-center mt-10">
+                        View More
+                    </button>
                 </div>
 
             </div>
--- src/Components/Pages/Shared/SectionTitle.jsx
+++ src/Components/Pages/Shared/SectionTitle.jsx
@@ -1,8 +1,17 @@
 const SectionTitle = ({ heading, subHeading }) => {
     return (
-        <div>
-
-        </div>
+        <>
+            <hr />
+            {/* Upcoming Events section*/}
+            <h2
+                className='text-center text-4xl font-black text-gray-600 pt-14'>
+                {heading}
+            </h2>
+            <p
+                className='text-base mt-5 text-yellow-400 font-black text-center'>
+                {subHeading}
+            </p>
+        </>
     );
 };
 
