09bd02780856b2d1c14af5f99282ee29b81b2084
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-11-08 12:42:21 +0600

    add google login and git hub login

--- src/Components/Utilites/Shared/SocialLogin.jsx
+++ src/Components/Utilites/Shared/SocialLogin.jsx
@@ -1,7 +1,49 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import useAuth from "../Hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+
 
 const SocialLogin = () => {
+    const { auth } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+    // google login function
+    const handleGoogleSignIn = () => {
+        const googleProvider = new GoogleAuthProvider();
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const google = result.user;
+                console.log(google);
+                navigate(from, { replace: true })
+                // todo react hot toast
+
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    // git hub login function
+    const gitHubLogin = () => {
+        const gitHubProvider = new GithubAuthProvider();
+        signInWithPopup(auth, gitHubProvider)
+            .then(result => {
+                const git = result.user;
+                console.log(git);
+                navigate(from, { replace: true })
+                // todo react hot toast
+
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+
     return (
         <>
             <h2 className="text-center text-2xl font-black text-yellow-400">
@@ -12,7 +54,7 @@ const SocialLogin = () => {
 
                 {/* google login start */}
                 <div
-                    // onClick={handleGoogleSignIn}
+                    onClick={handleGoogleSignIn}
                     className="col-span-6 flex items-center justify-end pr-5">
                     <FcGoogle
                         className='text-4xl' />
@@ -25,7 +67,7 @@ const SocialLogin = () => {
 
                 {/* github login start */}
                 <div
-                    // onClick={gitHubLogin}
+                    onClick={gitHubLogin}
                     className="col-span-6 flex items-center ps-5">
                     <FaGithub
 
